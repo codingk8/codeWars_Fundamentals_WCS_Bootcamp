27a975702a5b1606589b89b6f9e1fc1c963d8cd7
Catherine <codingk8@gmail.com>
2018-11-28 14:43:22 +0100

    Update 111_even_or_odd.js

--- 111_even_or_odd.js
+++ 111_even_or_odd.js
@@ -1,2 +1,15 @@
-/* Text */
+/* Kata: https://www.codewars.com/kata/even-or-odd
+See link in the MD file */
+
+/* ES5 */
+function even_or_odd(number) {
+  if (number %2 === 0){
+    return 'Even';
+  }
+  return 'Odd';
+}
+
+/* ES6 */
+const even_or_odd = n => n%2 === 0 ? 'Even' : 'Odd';
+
 
