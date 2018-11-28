c589b518b9132e73d3709d2ece7be61b13d0ec28
Catherine <codingk8@gmail.com>
2018-11-28 15:33:25 +0100

    Update 152_mumbling.js

--- 152_mumbling.js
+++ 152_mumbling.js
@@ -1,2 +1,16 @@
-/* CodeWars Kata: https://www.codewars.com/kata/even-or-odd
+/* CodeWars Kata: https://www.codewars.com/kata/mumbling
 See link & ressources in the MD file */
+
+/* ES5 */
+function accum(s) {
+	let l = s.toLowerCase().split('')
+  for(let i = 0; i < l.length; i++) {
+    l[i] = l[i].toUpperCase() + l[i].repeat(i)
+  }
+  return l.join('-')
+}
+
+/* Other ES5 */
+function accum(s) {
+  return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+}
