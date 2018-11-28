6798a645c2dc6435868ab1574b44671958cb0a93
Catherine <codingk8@gmail.com>
2018-11-28 15:03:42 +0100

    Update 122_jennys_secret_message.js

--- 122_jennys_secret_message.js
+++ 122_jennys_secret_message.js
@@ -1,2 +1,15 @@
-/* Kata: https://www.codewars.com/kata/even-or-odd
+/* CodeWars Kata: https://www.codewars.com/kata/jennys-secret-message
 See link & ressources in the MD file */
+
+/* ES5 */
+function greet(name){  
+  if(name === "Johnny")
+    return "Hello, my love!";
+  return "Hello, " + name + "!";
+}
+
+/* ES6 */
+const greet = n => "Hello, " + (n === "Johnny" ? "my love" : n ) + "!";
+
+/* ES6 upgrade */
+const h = n => 'Hello, ' + (n == 'Johnny' ? 'my love' : 'dear ' + n) + '!';
