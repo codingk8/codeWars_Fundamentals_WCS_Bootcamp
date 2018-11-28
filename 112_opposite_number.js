96d12bbe19df938f7d2a7faa962833c2798ef2b0
Catherine <codingk8@gmail.com>
2018-11-28 14:45:41 +0100

    Update 112_opposite_number.js

--- 112_opposite_number.js
+++ 112_opposite_number.js
@@ -1,1 +1,10 @@
-/* text */
+/* Kata: https://www.codewars.com/kata/opposite-number
+See link & ressources in the MD file */
+
+/* ES5 */
+function opposite(number) {
+  return -number;
+}
+
+/* ES6 */
+const opposite = n => -n;
