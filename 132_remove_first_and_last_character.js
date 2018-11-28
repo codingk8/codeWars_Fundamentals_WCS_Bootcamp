9eccf7995d88711b46f64eea4de4acbcb71e0752
Catherine <codingk8@gmail.com>
2018-11-28 15:10:15 +0100

    Update 132_remove_first_and_last_character.js

--- 132_remove_first_and_last_character.js
+++ 132_remove_first_and_last_character.js
@@ -1,2 +1,21 @@
-/* Kata: https://www.codewars.com/kata/even-or-odd
+/* CodeWars Kata: https://www.codewars.com/kata/remove-first-and-last-character
 See link & ressources in the MD file */
+
+/* ES5 */
+function removeChar(str){
+ return str.slice(1, -1);
+}
+
+function removeChar(str){
+ return str.substring(1, str.length-1);
+}
+
+function removeChar(str){
+  str1 = str.split('');
+  str1.shift();
+  str1.pop();
+  return str1.join(''); 
+}
+
+/* ES6 */
+const removeChar = str => str.slice(1,-1)
