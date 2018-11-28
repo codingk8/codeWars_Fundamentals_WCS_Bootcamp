ef6ef4a458bac6087ccbce69584e9f659cb2d388
Catherine <codingk8@gmail.com>
2018-11-28 15:16:50 +0100

    Update 142_get_middle_character.js

--- 142_get_middle_character.js
+++ 142_get_middle_character.js
@@ -1,2 +1,20 @@
-/* CodeWars Kata: https://www.codewars.com/kata/even-or-odd
+/* CodeWars Kata: https://www.codewars.com/kata/get-the-middle-character
 See link & ressources in the MD file */
+
+/* ES5 */
+ function getMiddle(s) {
+  return s.length % 2 ? s.substr(s.length / 2 , 1) : s.substr(s.length / 2 - 1, 2);
+}
+
+/* ES5 */
+function getMiddle(string) {
+  let middleIndex = string.length / 2;
+  if (string.length % 2 == 0) {
+    return string.slice(middleIndex - 1, middleIndex + 1);
+  } else {
+    return string.charAt(middleIndex);
+  }
+}
+
+/* ES6 */
+const getMiddle = s => (s.length % 2 ? s.substr(s.length / 2 , 1) : s.substr(s.length / 2 - 1, 2));
