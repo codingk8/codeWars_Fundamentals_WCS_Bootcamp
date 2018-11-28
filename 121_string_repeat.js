dc72bcf9a00b1602cd8e27dce3edb8dd2b297c2d
Catherine <codingk8@gmail.com>
2018-11-28 14:59:14 +0100

    Update 121_string_repeat.js

--- 121_string_repeat.js
+++ 121_string_repeat.js
@@ -1,2 +1,20 @@
-/* Kata: https://www.codewars.com/kata/even-or-odd
+/* CodeWars Kata: https://www.codewars.com/kata/string-repeat
 See link & ressources in the MD file */
+
+/* ES5 */
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+/* ES5 with a loop */
+function repeatStr (n, s) {
+var str="";
+for(var i=0; i < n; i++)
+  str+=s;
+  return str;
+}
+repeatStr(7, "g");
+
+
+/* ES6 */
+const repeatStr = (n, s) => s.repeat(n)
