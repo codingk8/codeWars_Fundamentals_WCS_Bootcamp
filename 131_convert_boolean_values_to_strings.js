0985fce46da6099fcce80de7d077452ac815d51f
Catherine <codingk8@gmail.com>
2018-11-28 15:05:36 +0100

    Update 131_convert_boolean_values_to_strings.js

--- 131_convert_boolean_values_to_strings.js
+++ 131_convert_boolean_values_to_strings.js
@@ -1,2 +1,13 @@
-/* Kata: https://www.codewars.com/kata/even-or-odd
+/* Kata: https://www.codewars.com/kata/convert-boolean-values-to-strings-yes-or-no
 See link & ressources in the MD file */
+
+/* ES 5 */
+function boolToWord(bool){
+  if (bool == true) {
+  return 'Yes'; 
+  }
+  return 'No'; 
+}
+
+/* ES6 */
+const boolToWord = bool => bool ? 'Yes' : 'No';
