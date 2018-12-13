9c324f9d46450767ba0f826e2ca954ab951c0230
Catherine <codingk8@gmail.com>
2018-12-13 20:28:43 +0100

    Update 162_duplicate_encoder.js

--- 162_duplicate_encoder.js
+++ 162_duplicate_encoder.js
@@ -1,1 +1,41 @@
-/* xxx */
+/* CodeWars Kata: https://www.codewars.com/kata/duplicate-encoder
+See link & ressources in the MD file */
+
+
+/* Ma solution */
+
+ let newWord = "";
+    let w = word.toLowerCase();
+
+    for (let i = 0; i < w.length; i++) {
+        if (w.indexOf(w[i]) === w.lastIndexOf(w[i])) {
+            newWord+="(";
+        } else {
+            newWord+=")";
+        }
+    }
+  return newWord;
+}
+
+/* Autres solutions */
+
+function duplicateEncode(word){
+  return word
+    .toLowerCase()
+    .split('')
+    .map( function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+    })
+    .join('');
+}
+
+function duplicateEncode(word) {
+  word = word.toLowerCase();
+  return word.replace(/./g, m => word.indexOf(m) == word.lastIndexOf(m) ? '(' : ')');
+}
+
+function duplicateEncode(word){
+    return word.toLowerCase().replace(/./g, function(match) { return word.toLowerCase().split(match).length > 2 ? ')' : '(' ;});
+}
+
+/* That's all folks! */
