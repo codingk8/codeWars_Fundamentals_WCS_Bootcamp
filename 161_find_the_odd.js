073c16e389d6633cd6d780b1762eea7c94667f43
Catherine <codingk8@gmail.com>
2018-12-13 19:32:51 +0100

    Update 161_find_the_odd.js

--- 161_find_the_odd.js
+++ 161_find_the_odd.js
@@ -1,4 +1,5 @@
-/* xxx */
+/* CodeWars Kata: https://www.codewars.com/kata/find-the-odd-int/train/javascript
+See link & ressources in the MD file */
 
 /* Ma solution */
 function findOdd(A) {
@@ -14,3 +15,41 @@ function findOdd(A) {
      }
   }
 }
+
+
+/* Autres solutions */
+
+const findOdd = A => A.filter(x => A.filter(v => x === v).length % 2 === 1).reduce(a => a);
+
+function findOdd(A) {
+  for(var i = 0; i < A.length; i++){  
+    //Query the number of times that this 'i' element appears
+    //then verify if that number of times is odd. If it is true, then return
+    //that 'i' element
+    if((A.filter(function(item){return item == A[i]; })).length % 2 != 0){
+      return A[i];
+    }
+  }
+  return 0;
+}
+
+function findOdd(arr) {
+  return arr.find((item, index) => arr.filter(el => el == item).length % 2)
+}
+
+/* A creuser, la signification du carte en JS */
+function findOdd(A) {
+  return A.reduce((l, r) => l ^ r);
+}
+
+function findOdd(A) {
+  var odd;
+  A.forEach( function(item, i, arr) {
+    if ( A.filter( val =>  val === item ).length % 2 !== 0) {
+     odd = item;
+    }
+  });
+  return odd;
+}
+
+/* That's all folks! */
