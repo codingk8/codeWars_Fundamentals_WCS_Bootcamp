401c77162310cd441f28e1840fbd96fc3b6597a4
Catherine <codingk8@gmail.com>
2018-12-13 18:42:59 +0100

    Update 152_mumbling.js

--- 152_mumbling.js
+++ 152_mumbling.js
@@ -1,6 +1,19 @@
 /* CodeWars Kata: https://www.codewars.com/kata/mumbling
 See link & ressources in the MD file */
 
+/* 20181213 Complément avec un map complet en ES5 */
+function f (s) {
+    return s
+        .toLowerCase()
+        .split("")
+        .map(function (item, index) {
+            return item[0].toUpperCase() + item[0].repeat(index);
+        })
+        .join("-");
+}
+    
+f("thfJnHkIlP");      
+
 /* ES5 */
 function accum(s) {
 	let l = s.toLowerCase().split('')
