26fab0ac646106de7fa57d433743a20aa5dd3d9e
Catherine <codingk8@gmail.com>
2018-12-15 16:37:32 +0100

    Update 111_even_or_odd.js

--- 111_even_or_odd.js
+++ 111_even_or_odd.js
@@ -1,5 +1,5 @@
-/* Kata: https://www.codewars.com/kata/even-or-odd
-See link & ressources in the MD file */
+/* CodeWars "Even or odd"
+See links & ressources in the MD file */
 
 /* ES5 */
 function even_or_odd(number) {
